0fa5d339358e77e1015b58b031691cb29feae843
Theophilus <theophilusadesola002@gmail.com>
2025-09-22 17:45:44 +0100

    Dynamic market page.tsx

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
         config.resolve.alias['@react-native-async-storage/async-storage'] = 'localforage'
         return config
     },
+    turbopack: {},
     productionBrowserSourceMaps: false,
 }
 
